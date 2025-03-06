4abdff07540fb68816835daa6586b9266b0643ec
neutr0gina-lab <gina.lee2@utsouthwestern.edu>
2025-03-06 15:14:51 -0600

    Update controller.py

--- src/pycalc/controller.py
+++ src/pycalc/controller.py
@@ -57,7 +57,7 @@ class PyCalcCtrl():
             None
         """
 
-        result = self._evaluate(expression=self._view.displayText()+"1")
+        result = self._evaluate(expression=self._view.displayText())
         self._view.setDisplayText(result)
         
     def _buildExpression(self, sub_exp):
